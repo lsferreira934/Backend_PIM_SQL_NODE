7215ec6a174ff05dd4b904be8586270164808f5d
FelipeLeonardo98 <felipeferrazleonardo98@gmail.com>
2020-11-22 02:44:44 -0300

    Felipe: env and AWS RDS

--- src/config/config.js
+++ src/config/config.js
@@ -1,32 +1,25 @@
 require('dotenv').config();
 
+
 module.exports = {
   development: {
     database: {
-      /*  host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        name: process.env.DB_NAME,
-        dialect: 'mysql',
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,*/
-      host: 'localhost',
-      port: 3306,
-      name: 'db_pim',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      name: process.env.DB_NAME,
       dialect: 'mysql',
-      user: 'root',
-      password: 'root'
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
   },
   production: {
     database: {
-      /* host: process.env.DB_HOST,
-       port: process.env.DB_PORT,
-       name: process.env.DB_NAME,
-       dialect: 'mysql',
-       user: process.env.DB_USER,
-       password: process.env.DB_PASSWORD,*/
       host: process.env.DB_HOST,
-      host: process.env.DB_PORT
+      port: process.env.DB_PORT,
+      name: process.env.DB_NAME,
+      dialect: 'mysql',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
   },
 };
