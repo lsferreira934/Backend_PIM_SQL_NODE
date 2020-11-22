2cd02f6b564692966bfaf572fb5fd6c85b081828
Leandro <leandrosf934@gmail.com>
2020-11-22 16:09:19 -0300

    conifg atualizado

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 
-
 module.exports = {
   development: {
     database: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       name: process.env.DB_NAME,
-      dialect: 'mysql',
+      dialect: process.env.DB_DIALECT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
     },
@@ -17,7 +16,7 @@ module.exports = {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       name: process.env.DB_NAME,
-      dialect: 'mysql',
+      dialect: process.env.DB_DIALECT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
     },
